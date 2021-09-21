dbaa450b31b02dfa5e22f15e0f994f6e85fd1a70
William <williambecker321@gmail.com>
2021-09-21 12:28:51 +0200

    Gort så att listan visar bilarna i systemet.

--- LINQ_och_Lamda_Cars/Form1.Designer.cs
+++ LINQ_och_Lamda_Cars/Form1.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace LINQ_och_Lamda_Cars
 {
-    partial class Form1
+    partial class Car_Tracker
     {
         /// <summary>
         /// Required designer variable.
@@ -29,13 +29,33 @@ namespace LINQ_och_Lamda_Cars
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lib_CarList = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // lib_CarList
+            // 
+            this.lib_CarList.FormattingEnabled = true;
+            this.lib_CarList.ItemHeight = 16;
+            this.lib_CarList.Location = new System.Drawing.Point(176, 12);
+            this.lib_CarList.Name = "lib_CarList";
+            this.lib_CarList.Size = new System.Drawing.Size(437, 212);
+            this.lib_CarList.TabIndex = 0;
+            // 
+            // Car_Tracker
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 661);
+            this.Controls.Add(this.lib_CarList);
+            this.Name = "Car_Tracker";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox lib_CarList;
     }
 }
 
